037df8bd21dd76988c185a95e0a7a480030fb7ce
Paul Bailey <paul.m.bailey@gmail.com>
2013-09-26 16:08:36 +0000

    pip setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+from setuptools import setup, find_packages
+
+MY_DIR = os.path.normpath(os.path.dirname(__file__))
+
+setup(
+  name = "gae_boto",
+  version = '13.09',
+  description = "A recreation of Amazon Boto library that is is compatible with Google App Engine and easier to use.",
+  url = "https://github.com/pizzapanther/gae_boto",
+  author = "Paul Bailey",
+  author_email = "paul.m.bailey@gmail.com",
+  license = "BSD",
+  packages = ['gae_boto', 'gae_boto.apis'],
+  package_dir = {'gae_boto': MY_DIR},
+  install_requires = [
+    "requests>=2.0.0"
+  ]
+)
