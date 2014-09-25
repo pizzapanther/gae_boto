0de2d83cae8ccc951cecb6cbfd81aadb9a36dd41
Paul Bailey <paul.m.bailey@gmail.com>
2014-09-25 17:22:12 -0400

    added unicode

--- apis/s3.py
+++ apis/s3.py
@@ -16,16 +16,16 @@ class S3Key (object):
     if https:
       http = 'https'
       
-    url = '{http}://{bucket}.s3.amazonaws.com/{key}'.format(http=http, bucket=self.bucket, key=self.key)
+    url = u'{http}://{bucket}.s3.amazonaws.com/{key}'.format(http=http, bucket=self.bucket, key=self.key)
     
     if sign:
       now = datetime.datetime.utcnow()
       expire = int(time.mktime(now.utctimetuple())) + expires
-      sign_me = "{method}\n\n\n{expire}\n/{bucket}/{key}".format(method=method, bucket=self.bucket, key=self.key, expire=expire)
+      sign_me = u"{method}\n\n\n{expire}\n/{bucket}/{key}".format(method=method, bucket=self.bucket, key=self.key, expire=expire)
       h = hmac.new(key=self.connection.aws_key, msg=sign_me, digestmod=hashlib.sha1)
       signature = base64.b64encode(h.digest()).decode()
       
-      url += "?AWSAccessKeyId={aws_id}&Expires={expire}&Signature={signature}".format(
+      url += u"?AWSAccessKeyId={aws_id}&Expires={expire}&Signature={signature}".format(
         aws_id=urllib.quote(self.connection.aws_id),
         expire=expire,
         signature=urllib.quote(signature),
